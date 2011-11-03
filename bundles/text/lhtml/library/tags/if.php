66bb17d14cfb9d83baa7b1248f1ced374ee02e05
Kelly Becker <kellylsbkr@gmail.com>
2011-11-03 12:03:43 -0400

    fixed some lhtml if issues

--- bundles/text/lhtml/library/tags/if.php
+++ bundles/text/lhtml/library/tags/if.php
@@ -11,17 +11,31 @@ class tag_if extends Node {
 	
 	public function build() {		
 		$this->_init_scope();
+		$output = "";
+		
+		/**
+		 * If requires iteration
+		 * else just execute the loop once
+		 */
+		if($this->is_loop) {
+			$this->_data()->reset();
+		} else {
+			$once = 1;
+		}
+		
+		/**
+		 * Start build loop
+		 */
+		while($this->is_loop ? $this->_data()->iteratable() : $once--) {
 		
 		/**
 		 * Render the code
 		 */
-		if($this->process()){
-		
+		if($this->process()) {
 			if(!empty($this->children)) foreach($this->children as $child) {			
 				if(is_object($child)) $output .= $child->output();
 				else if(is_string($child)) $output .= $this->_string_parse($child);
 			}
-		
 		}
 		else {
 			if(!empty($this->children)) foreach($this->children as $child) {	
@@ -31,31 +45,50 @@ class tag_if extends Node {
 			}
 		}
 		
+		/**
+		 * If a loop increment the pointer
+		 */
+		if($this->is_loop) $this->_data()->next();
+		
+		/**
+		 * End build loop
+		 */
+		}
+		
+		/**
+		 * Return the rendered page
+		 */
 		return $output;
 	}
 	
 	public function process() {
 		if(!isset($this->attributes['cond']))
-			throw new Exception('No if-condition `cond` specified');	
+			throw new Exception('No if-condition `cond` specified');
 		
 		$v = $this->attributes['cond'];
 		
 		$vars = $this->extract_vars($v);
 		if($vars) foreach($vars as $var) {
-			$data_response = $this->_data()->$var;	
+			$data_response = $this->_data()->$var;
 			$v = str_replace('{'.$var.'}', $data_response, $v);				
-		}				
+		}
+		
+		$v = str_replace('\'', '', $v);
 		
 		$v = explode(' ', $v);
 		
+		if($v[0] === '') $v[0] = null;
+		if($v[2] === '') $v[2] = null;
 		if($v[0] === 'true') $v[0] = true;
 		if($v[2] === 'true') $v[2] = true;
 		if($v[0] === 'false') $v[0] = false;
 		if($v[2] === 'false') $v[2] = false;
 		
-		if(!is_bool($v[0])) $v[0] = $this->_data()->$v[0];
-		if(!is_bool($v[2])) $v[2] = $this->_data()->$v[2];
-		
+		if(!is_bool($v[0]) && is_string($v[0])) $v[0] = $this->_data()->$v[0];
+		if(!is_bool($v[2]) && is_string($v[2])) $v[2] = $this->_data()->$v[2];
+
+		if($v[0] === null) $v[0] = 'null';
+		if($v[2] === null) $v[2] = 'null';
 		if($v[0] === true) $v[0] = 'true';
 		if($v[2] === true) $v[2] = 'true';
 		if($v[0] === false) $v[0] = 'false';
@@ -90,18 +123,46 @@ class tag_else extends Node {
 	
 	public function build() {
 		$this->_init_scope();
+		$output = "";
+		
+		/**
+		 * If requires iteration
+		 * else just execute the loop once
+		 */
+		if($this->is_loop) {
+			$this->_data()->reset();
+		} else {
+			$once = 1;
+		}
+		
+		/**
+		 * Start build loop
+		 */
+		while($this->is_loop ? $this->_data()->iteratable() : $once--) {
 		
 		/**
 		 * Render the code
 		 */
-		if($this->show_else){
+		if($this->show_else) {
 			if(!empty($this->children)) foreach($this->children as $child) {			
 				if(is_object($child)) $output .= $child->output();
 				else if(is_string($child)) $output .= $this->_string_parse($child);
 			}
-		
 		}
 		
+		/**
+		 * If a loop increment the pointer
+		 */
+		if($this->is_loop) $this->_data()->next();
+		
+		/**
+		 * End build loop
+		 */
+		}
+		
+		/**
+		 * Return the rendered page
+		 */
 		return $output;
 	}
 	
