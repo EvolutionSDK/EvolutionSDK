7e7d0d24ad0578ec06fa94d224e6ea2bed48234d
Nate Ferrero <nateferrero@gmail.com>
2011-11-13 18:06:30 -0800

    Env vars upgrade, just for you Kelly

--- bundles/system/environment/library/manage.php
+++ bundles/system/environment/library/manage.php
@@ -12,18 +12,43 @@ class Manage {
 	public $title = 'Environment';
 	
 	public function page($path) {
-		$out = '<div class="section"><h1>Global Variables</h1>';
+		$out = '';
 		$env = Bundle::getAll();
+		$categories = array();
+		
 		foreach($env as $key => $value) {
 			if(substr($key, -9) === '---format')
 				continue;
+			$tmp = explode('.', $key);
+			if(isset($tmp[3])) {
+				$category = "$tmp[0].$tmp[1].$tmp[2]";
+				array_shift($tmp);
+				array_shift($tmp);
+				array_shift($tmp);
+				$name = implode('.', $tmp);
+			} else {
+				$category = 'Misc';
+				$name = $key;
+			}
+			if(!isset($categories[$category]))
+				$categories[$category] = array();
+			
 			$delete = '/@environment/delete/' . base64_encode($key);
 			$delete = '<a href="'.$delete.'">Delete</a>';
 			$edit = '/@environment/edit/' . base64_encode($key);
 			$edit = '<a href="'.$edit.'">Edit</a>';
-			$out .= "<div class='var'>$edit &bull; $delete &bull; <code class='alt'>$key</code> = <code>$value</code></div>";
+			
+			/**
+			 * Save the HTML
+			 */
+			$categories[$category][$name] = "<div class='var'><h3>$name</h3>$edit &bull; $delete &bull; <code>$value</code></div>";
 		}
-		$out .= '</div>';
+		
+		foreach($categories as $category => $items) {
+			$out .= '<div class="section"><h1>'.$category.'</h1>';
+			$out .= implode('', $items);
+			$out .= '</div>';
+		}
 		return $out;
 	}
 	
