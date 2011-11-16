4782a060a88faa2fb6691961c9dfaf2536cf4e72
Nate Ferrero <nateferrero@gmail.com>
2011-11-16 15:21:46 -0800

    Un dump less

--- bundles/text/less/_bundle.php
+++ bundles/text/less/_bundle.php
@@ -11,8 +11,6 @@ use e;
 class Bundle {
 	public function __invoke_bundle($file) {
 		require_once(__DIR__ . '/library/lessc.inc.php');
-		$cfile = e::cache()->path() . '/less/' . md5($file) . '.css';
-		dump($cfile);
-		return ;
+		return e::cache()->path() . '/less/' . md5($file) . '.css';
 	}
 }
