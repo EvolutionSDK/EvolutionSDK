2f644449dce1d495817b332068a185b55217dee9
Kelly Becker <kellylsbkr@gmail.com>
2011-11-28 01:33:48 -0800

    added the ability to call actions

--- bundles/logic/action/_bundle.php
+++ bundles/logic/action/_bundle.php
@@ -16,6 +16,72 @@ class Bundle {
 		$this->route($path, e::configure('action')->locations);
 	}
 	
+	public function load($action, $data) {
+		if(is_array($action)) {
+			$dirs = array(e::$site.'/portals/'.array_shift($action));
+			$name = str_replace('\\', '/', strtolower(array_shift($action)));
+		}
+		else $name = str_replace('\\', '/', strtolower($action));
+		
+		// Add defaults
+		e::configure('action')->activeAdd('class_format', '\\Action\\%');
+		e::configure('action')->activeAdd('locations', e::$site);
+		
+		// If dirs are not specified, use defaults
+		if(!isset($dirs))
+			$dirs = e::configure('action')->locations;
+		
+		// Check all dirs for a matching action
+		foreach($dirs as $dir) {
+			// Look in action folder
+			if(basename($dir) !== 'actions')
+				$dir .= '/actions';
+			
+			// Skip if missing
+			if(!is_dir($dir))
+				continue;
+				
+			// File to check
+			$file = "$dir/$name.php";
+			
+			// Skip if incorrect file
+			if(!is_file($file))
+				continue;
+			
+			// Require the controller
+			require_once($file);
+				
+			// action class
+			$classFormats = e::configure('action')->class_format;
+			
+			// Check each class format
+			$found = false;
+			foreach($classFormats as $format) {
+				
+				// Format class with action name
+				$class = str_replace(array('%', '/'), array($name, '\\'), $format);
+				
+				// Check if this is a valid class
+				if(class_exists($class, false)) {
+					$found = true;
+					break;
+				}
+			}
+			
+			// Maybe we just ran out of formats to check
+			if(!$found) {
+				$classes = implode('`, `', $classFormats);
+				$classes = str_replace('%', $name, $classes);
+				throw new Exception("None of the possible action classes: `$classes` are defined in `$file`");
+			}
+			
+			// Load action
+			return new $class($data);
+			
+		}
+		
+	}
+	
 	public static function route($path, $dirs = null) {
 		
 		// Add defaults
