511af8ff221b4c359acbd120eb441558b300a43a
Kelly Becker <kellylsbkr@gmail.com>
2011-12-29 12:28:44 -0600

    save

--- bundles/text/lhtml/library/tags/if.php
+++ bundles/text/lhtml/library/tags/if.php
@@ -176,6 +176,22 @@ class Node_if extends Node {
 			
 		}
 		
+		if(isset($this->attributes['not_empty'])) {
+		
+			$v = $this->attributes['not_empty'];
+			
+			if(strpos($v, '{') === false) $v = '{'.$v.'}';
+
+			$vars = $this->extract_vars($v);
+			if($vars) foreach($vars as $var) {
+				$v = $this->_data()->$var;
+			}
+			
+			if(!empty($v)) $retval = true;
+			else $retval = false;
+			
+		}
+		
 		if(isset($retval) && !$retval) foreach($this->children as $child) {
 			if(isset($child->fake_element) && $child->fake_element == ':else') $child->show_else = 1;
 		}
