f382460195ffe01a7b9035a88a589a1d99122ff0
Nate Ferrero <nateferrero@gmail.com>
2012-01-20 18:29:43 -0700

    Streamlining Evolution, render exception

--- kernel/functions.php
+++ kernel/functions.php
@@ -34,6 +34,50 @@ function plural($num, $word = 'item/s', $empty = '0', $chr = '/') {
 			($num === 1 ? '' : $word[1])
 		);
 }
+/**
+ * Render an exception
+ */
+function render_exception(&$exception) {
+	
+	// Get message
+	$message = $exception->getMessage();
+	$message = preg_replace('/`([^`]*)`/x', '<code>$1</code>', $message);
+	
+	$out = "<div class='section'><h1>Uncaught ".get_class($exception)."</h1>";
+	
+	// Show message
+	if(strlen($message) > 1)
+		$out .= "<p>$message</p>";
+	
+	/**
+	 * Start reveal div
+	 */	
+	$out .= "<div class='reveal'>";
+	
+	$out .= "<p>Error happened on <span class='line'>line " . $exception->getLine() .
+		'</span> of <code class="file">' . $exception->getFile() . '</code></p>';
+	
+	/**
+	 * Show stack trace
+	 */
+	$out .= stylize_stack_trace($exception->getTrace());
+	
+	/**
+	 * End reveal div
+	 */
+	$out .= '</div>';
+	
+	// Check for previous exception
+	$prev = $exception->getPrevious();
+	if(is_object($prev))
+		$out .= render_exception($prev);
+		
+	// Close section
+	$out .= '</div>';
+	
+	// Return output
+	return $out;
+}
 
 /**
  * Time since filter
