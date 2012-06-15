15a818330d555b045c84fd329eec81595e45c875
Kelly Becker <kellylsbkr@gmail.com>
2012-06-15 17:08:37 -0600

    nginx hacks

---
+++ kernel/hacks/nginx.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Hack for apache_request_headers() and getallheaders() on nginx
+ * @author Kelly Becker
+ */
+if (!function_exists('apache_request_headers')) {
+	function apache_request_headers() {
+		foreach($_SERVER as $key=>$value) {
+			if (substr($key,0,5)=="HTTP_") {
+				$key = str_replace(" ", "-", ucwords(strtolower(str_replace("_", " ", substr($key,5)))));
+				$out[$key] = $value;
+			} else $out[$key]=$value;
+		}
+		
+		return $out;
+	}
+}
+
+if(!function_exists('getallheaders')) {
+	function getallheaders() {
+		return apache_request_headers();
+	}
+}
